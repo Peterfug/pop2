f8d96c1c70f01d66b829441a02568b942dea09d4
Peterfug <fugedi.peter5@gmail.com>
2022-08-04 21:22:42 +0200

    lvl 3 done

--- level3-write-your-code-here.js
+++ level3-write-your-code-here.js
@@ -12,21 +12,75 @@
 
     HINT: use the BROWSER DEVELOPER TOOLS.
 */
-
+let princeX = 0;
+let princeY = 0;
 function level3Move(gameMap) {
-    /* console.dir(gameMap);
+  /* console.dir(gameMap);
     console.log(gameMap[0]);
     console.log(gameMap[0].length);
     console.log(gameMap[2][1]); */
-    
-    let numberOfDown = [];
-    for (let i = 0; i < gameMap.length - 3; i++) {
-        numberOfDown.push('down');
-    };
-    for (let i = 0; i < gameMap[0].length -3; i++) {
-        numberOfDown.push('right');
-    };
-    console.log(numberOfDown);
-    return numberOfDown;
+
+  let numberOfDown = [];
+  for (let i = 0; i < gameMap.length - 3; i++) {
+    numberOfDown.push("down");
+  }
+  for (let i = 0; i < gameMap[0].length - 3; i++) {
+    numberOfDown.push("right");
+  }
+  princeY = gameMap.length - 2;
+  princeX = gameMap[0].length - 2;
+  console.log(princeY);
+  console.log(princeX);
+  console.log(numberOfDown);
+
+  while (gameMap[princeY - 1][princeX] !== 13) {
+    moveUp(numberOfDown);
+  }
+
+  while (gameMap[princeY][princeX - 1] === 11) {
+    zigZag(numberOfDown);
+    if (gameMap[princeY - 1][princeX - 1] === 11) {
+      break;
+    }
+  }
+
+  for (let index = 0; index < 51; index++) {
+    toPrincess(numberOfDown);
+  }
+  return numberOfDown;
+}
+function toPrincess(numberOfDown) {
+  MoveLeft(numberOfDown);
+  MoveDown(numberOfDown);
+  MoveRight(numberOfDown);
+  MoveDown(numberOfDown);
 }
 
+function zigZag(numberOfDown) {
+  MoveLeft(numberOfDown);
+  MoveLeft(numberOfDown);
+  moveUp(numberOfDown);
+  MoveLeft(numberOfDown);
+  MoveLeft(numberOfDown);
+  MoveDown(numberOfDown);
+}
+
+function moveUp(numberOfDown) {
+  numberOfDown.push("up");
+  princeY = princeY - 1;
+}
+
+function MoveLeft(numberOfDown) {
+  numberOfDown.push("left");
+  princeX = princeX - 1;
+}
+
+function MoveRight(numberOfDown) {
+  numberOfDown.push("right");
+  princeX = princeX + 1;
+}
+
+function MoveDown(numberOfDown) {
+  numberOfDown.push("down");
+  princeY = princeY + 1;
+}
