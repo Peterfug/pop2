6d49ca5b8cab7c51d46933182b2601f7b5da0ac4
Peterfug <fugedi.peter5@gmail.com>
2022-08-04 13:34:53 +0200

    zig zag elott

--- level2-write-your-code-here.js
+++ level2-write-your-code-here.js
@@ -13,60 +13,109 @@
 
     HINT: use the BROWSER DEVELOPER TOOLS.
 */
+let firstTask = true;
+let secondTask = true;
+let thirdTask = true;
+let fourthTask = true;
+function level2Move(
+  elementLeftOfPrince,
+  elementRightOfPrince,
+  elementUpOfPrince,
+  elementDownOfPrince
+) {
+  if (firstTask) {
+    down(elementDownOfPrince);
+  }
+  if (secondTask) {
+    right(elementRightOfPrince);
+  }
+  if (thirdTask) {
+    up(elementUpOfPrince);
+  }
+  if (fourthTask) {
+    zigZag(elementLeftOfPrince,);
+  }
 
-let numberOfDown = 0;
-let numberOfRight = 0;
+  // the prince should move down first, the problem is that the amount
+  // of times to move changes from one level to another. Try to find out
+  // based on the variables "elementLeftOfPrince", "elementRightOfPrince",
+  // "elementUpOfPrince", "elementDownOfPrince"
 
-function level2Move(elementLeftOfPrince, elementRightOfPrince, elementUpOfPrince, elementDownOfPrince) {
-    
-    moveDown(elementDownOfPrince);
-    moveRight(elementRightOfPrince);
-    //moveUp(elementUpOfPrince);
-    
-    // the prince should move down first, the problem is that the amount
-    // of times to move changes from one level to another. Try to find out
-    // based on the variables "elementLeftOfPrince", "elementRightOfPrince",
-    // "elementUpOfPrince", "elementDownOfPrince"
+  // the prince should move right second, the problem is that the amount
+  // of time to move changes from one level to another
+  // HINT: try to create a function that would receive the parameters
+  // up/down/left/right and the amount of times to move
 
+  // the prince should move up third, the problem is that the amount
+  // of time to move changes from one level to another
+  // HINT: try to create a function that would receive the parameters
+  // up/down/left/right and the amount of times to move
 
-    // the prince should move right second, the problem is that the amount
-    // of time to move changes from one level to another
-    // HINT: try to create a function that would receive the parameters
-    // up/down/left/right and the amount of times to move
+  // the prince should move in a zig-zag pattern. Unfortunately the amount
+  // of times changes from one level to another
+  // HINT: try to create a function that would receive the parameters
 
-
-    // the prince should move up third, the problem is that the amount
-    // of time to move changes from one level to another
-    // HINT: try to create a function that would receive the parameters
-    // up/down/left/right and the amount of times to move
-
-    // the prince should move in a zig-zag pattern. Unfortunately the amount
-    // of times changes from one level to another
-    // HINT: try to create a function that would receive the parameters
-
-    // to finally reach the princess the prince should move down and sometimes
-    // to left or right, depending on the level
+  // to finally reach the princess the prince should move down and sometimes
+  // to left or right, depending on the level
 }
 
+function down(elementDownOfPrince) {
+  if (elementDownOfPrince < 12) {
+    moveDirection("down");
+    firstTask = true;
+    secondTask = false;
+    thirdTask = false;
+    fourthTask = false;
+  } else {
+    firstTask = false;
+    secondTask = true;
+    thirdTask = false;
+    fourthTask = false;
+  }
+}
 
-function moveDown(elementDownOfPrince) {
-    if (elementDownOfPrince === 11) {
-        moveDirection('down');
-        numberOfDown += 1;
-    };
-    console.log(numberOfDown);
-};
+function right(elementRightOfPrince) {
+  if (elementRightOfPrince < 12) {
+    moveDirection("right");
+    firstTask = false;
+    secondTask = true;
+    thirdTask = false;
+    fourthTask = false;
+  } else {
+    firstTask = false;
+    secondTask = false;
+    thirdTask = true;
+    fourthTask = false;
+  }
+}
 
-function moveRight(elementRightOfPrince) {
-    if (elementRightOfPrince === 11) {
-        moveDirection('right');
-        numberOfRight += 1;
-    };
-    console.log(numberOfRight);
-};
- 
-/* function moveUp(elementUpOfPrince) {
-    if (elementUpOfPrince === 11 && functionRun > 20) {
-        moveDirection('up');
-    };
-}; */
+function up(elementUpOfPrince) {
+  if (elementUpOfPrince < 12) {
+    moveDirection("up");
+    firstTask = false;
+    secondTask = false;
+    thirdTask = true;
+    fourthTask = false;
+  } else {
+    firstTask = false;
+    secondTask = false;
+    thirdTask = false;
+    fourthTask = true;
+  }
+}
+
+function zigZag(elementLeftOfPrince,) {
+  if (elementLeftOfPrince < 12) {
+    moveDirection("left");
+
+    firstTask = false;
+    secondTask = false;
+    thirdTask = false;
+    fourthTask = true;
+  } else {
+    firstTask = false;
+    secondTask = false;
+    thirdTask = true;
+    fourthTask = true;
+  }
+}
