c2f137a17cbb33d46c6ea882597392f522aa21a6
akosnandornagy <109482995+akosnandornagy@users.noreply.github.com>
2022-08-05 01:25:49 +0200

    Update level2-write-your-code-here.js

--- level2-write-your-code-here.js
+++ level2-write-your-code-here.js
@@ -13,40 +13,8 @@
 
     HINT: use the BROWSER DEVELOPER TOOLS.
 */
-let firstTask = true;
-let secondTask = true;
-let thirdTask = true;
-let fourthTask = true;
-let fifthTask = true;
-function level2Move(
-  elementLeftOfPrince,
-  elementRightOfPrince,
-  elementUpOfPrince,
-  elementDownOfPrince
-) {
-  console.log(
-    `MOST van körülötted le:${elementDownOfPrince} fel:${elementUpOfPrince} jobbra:${elementRightOfPrince} balra:${elementLeftOfPrince}`
-  );
-  if (firstTask) {
-    down(elementDownOfPrince);
-  }
-  if (secondTask) {
-    right(elementRightOfPrince);
-  }
-  if (thirdTask) {
-    up(elementUpOfPrince);
-  }
-  if (fourthTask) {
-    ZigZag(elementDownOfPrince, elementUpOfPrince);
-  }
-  console.log(
-    `MI van körülötted le:${elementDownOfPrince} fel:${elementUpOfPrince} jobbra:${elementRightOfPrince} balra:${elementLeftOfPrince}`
-  );
-  if (fifthTask){
-    princess()
-  }
 
-  // the prince should move down first, the problem is that the amount
+      // the prince should move down first, the problem is that the amount
   // of times to move changes from one level to another. Try to find out
   // based on the variables "elementLeftOfPrince", "elementRightOfPrince",
   // "elementUpOfPrince", "elementDownOfPrince"
@@ -68,102 +36,17 @@ function level2Move(
   // to finally reach the princess the prince should move down and sometimes
   // to left or right, depending on the level
 
-}
 
 function down(elementDownOfPrince) {
-  if (elementDownOfPrince < 12) {
-    moveDirection("down");
-    firstTask = true;
-    secondTask = false;
-    thirdTask = false;
-    fourthTask = false;
-    fifthTask = false;
   } else {
-    firstTask = false;
-    secondTask = true;
-    thirdTask = false;
-    fourthTask = false;
-    fifthTask = false;
-  }
-}
 
 function right(elementRightOfPrince) {
-  if (elementRightOfPrince < 12) {
     moveDirection("right");
-    firstTask = false;
-    secondTask = true;
-    thirdTask = false;
-    fourthTask = false;
-    fifthTask = false;
   } else {
-    firstTask = false;
-    secondTask = false;
-    thirdTask = true;
-    fourthTask = false;
-    fifthTask = false;
-  }
-}
 
 function up(elementUpOfPrince) {
-  if (elementUpOfPrince < 12) {
     moveDirection("up");
-    firstTask = false;
-    secondTask = false;
-    thirdTask = true;
-    fourthTask = false;
-    fifthTask = false;
   } else {
-    firstTask = false;
-    secondTask = false;
-    thirdTask = false;
-    fourthTask = true;
-    fifthTask = false;
-  }
-}
 
-function ZigZag(elementDownOfPrince, elementUpOfPrince) {
-  if (elementDownOfPrince === 11 && elementUpOfPrince === 11) {
-    firstTask = false;
-    secondTask = false;
-    thirdTask = false;
-    fourthTask = false;
-    fifthTask = true;
-  } else {
-    moveDirection("left");
-    console.log("most balra lépek");
-    moveDirection("left");
-    console.log("most balra lépek");
-    moveDirection("up");
-    console.log("most fel lépek");
-    moveDirection("left");
-    console.log("most balra lépek");
-    moveDirection("left");
-    console.log("most balra lépek");
-    moveDirection("down");
-    console.log("most lefele lépek");
 
-    firstTask = false;
-    secondTask = false;
-    thirdTask = false;
-    fourthTask = true;
-    fifthTask = false;
-  }
-}
-
-function princess() {
-  for (let index = 0; index < 50; index++) {
-    moveDirection("left");
-    moveDirection("down");
-    moveDirection("right");
-    moveDirection("down");
-    firstTask = false;
-    secondTask = false;
-    thirdTask = false;
-    fourthTask = false;
-    fifthTask = true;
-  }
- 
-  
-  
-}
 
